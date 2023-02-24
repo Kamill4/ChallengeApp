d7f5d8b1482e0e7745d99a60de079d36d687f593
Kamil <kamilburek02@gmail.com>
2023-02-24 23:35:35 +0100

    Zadanie domowe 4

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -1,3 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
- var
+﻿
+
+using System.Xml.Linq;
+
+var myAge = 22;
+var myName = "Kamil";
+var Men = true;
+
+
+if (myName == "Kamil" || myAge == 22)
+{
+    Console.WriteLine(myName + " lat" + myAge + "Płeć" + Men);
+}
+else if (myAge < 13) 
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+        }
+else if (Men == false )
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (myName == "Ewa" && myAge == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
